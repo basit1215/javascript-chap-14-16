0dbeec73aa6fb83a3e4673de7a6a880d924467cd
basit1215 <haizabdulbasit225@gmail.com>
2024-03-18 08:26:41 +0500

    Update app.js

--- app.js
+++ app.js
@@ -30,10 +30,55 @@
 // }
 
 
-var ColorsNames=["Red", "Blue","Yellow","Orange", "Purple", "Brown", "Pink", "Blue", "Green"];
-for(i=0; i<ColorsNames.length; i++){
-   document.write(ColorsNames[i] + "<br>" );
-}
-var colorAdd= prompt("Enter color name you want to add in the beginning of array");
-ColorsNames.unshift(colorAdd);
-document.write("Updated Color is: " + ColorsNames  + "<br>" );
+// var ColorsNames=["Red", "Blue","Yellow","Orange", "Purple", "Brown", "Pink", "Blue", "Green"];
+// for(i=0; i<ColorsNames.length; i++){
+//    document.write(ColorsNames[i] + "<br>" );
+// }
+
+// var colorAdd1= prompt("Enter color name you want to add in the beginning of array");
+// ColorsNames.unshift(colorAdd1);
+// document.write("Updated Color is: " + ColorsNames  + "<br>" );
+
+// var colorAdd2= prompt("Enter color name you want to add in the end of array");
+// ColorsNames.push(colorAdd2);
+// document.write("Updated Color is: " + ColorsNames + "<br>" );
+
+// ColorsNames.unshift("Gray", "Light Green");
+// document.write("Updated Color is: " + ColorsNames  + "<br>"  );
+
+// ColorsNames.shift();
+// document.write("Updated Color is: " + ColorsNames + "<br>" );
+
+// ColorsNames.pop();
+// document.write("Updated Color is: " + ColorsNames + "<br>"  );
+
+// var indexColorAdd = +prompt("Enter index number where you want to Add color");
+// var colorAdd3= prompt("Enter color name you want to Add");
+// ColorsNames.splice(indexColorAdd, 0, colorAdd3);
+// document.write("Updated Color is: " + ColorsNames + "<br>" );
+
+// var indexColorDel= +prompt("Enter index number where you want to Delete color");
+// var colorDelNum= +prompt("Enter number of colors you want to delete");
+// ColorsNames.splice(indexColorDel, colorDelNum);
+// document.write("Updated Color is: " + ColorsNames + "<br>"  );
+
+// var studentsScore= [450, 380, 330, 500, 215, 160];
+// document.write("Scores of Students: " + studentsScore + "<br>"  );
+
+// studentsScore.sort();
+// document.write("Odered Scores of Students: " + studentsScore + "<br>"  );
+
+//  var cities= ["Karachi", "Lahore", "Islamabad", "Quetta", "Peshawar", "Multan"];
+//  document.write("Cities List: ");
+//  document.write(cities+ "<br>")  ;
+
+//  var selectedCities= cities.slice(1, 5);
+//  document.write("Selected Cities List: ");
+//  document.write(selectedCities+ "<br>" );
+
+ var arr1= ["This " , "is ", "my ", "cat."];
+ document.write(arr1+ "<br>");
+
+ var arr2= arr1.join("");
+ document.write(arr2+ "<br>");
+
